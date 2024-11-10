d10c3089dcfd5b20c9c54fe905cd5c49ec282d7c
bonnerjallah <121054718+bonnerjallah@users.noreply.github.com>
2024-11-09 23:18:07 -0500

    modified backend

--- Backend/server.js
+++ Backend/server.js
@@ -6,18 +6,26 @@ const connectDB = require("./config/db")
 const router = require("./routes/router")
 const cookieParser = require("cookie-parser")
 
-const frontEndUrl = process.env.FRONTEND_URL
 const PORT = process.env.PORT || 3001
+const allowedOrigins = [
+    process.env.ADMIN_FRONTEND_URL, 
+    process.env.CLIENT_FRONTEND_URL 
+];
 
+const app = express();
 
-const app = express()
-
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use(cors({
-    origin: `${frontEndUrl}`,
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true); // Allow the request
+        } else {
+            callback(new Error("Not allowed by CORS")); // Error for disallowed origins
+        }
+    },
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }))
